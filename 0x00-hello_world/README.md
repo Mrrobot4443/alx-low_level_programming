7e7a824f238d68702de781c0cfbb5712bcfb2987
YASSINE BESS <yazo-yazo@hotmail.com>
2023-06-15 13:05:09 -0700

    preprocessor -E

---
+++ 0x00-hello_world/README.md
@@ -0,0 +1,1 @@
+'HELLO'__'WORLD
---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: 0 to indicate successful program execution
+ *     */
+int main(void)
+{
+	    return (0);
+}
+
