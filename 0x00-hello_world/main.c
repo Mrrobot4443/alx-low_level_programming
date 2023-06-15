540b49dd3fd983ca589a96da1531d8da7aed2a83
YASSINE BESS <yazo-yazo@hotmail.com>
2023-06-15 13:33:28 -0700

    -c

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -3,10 +3,9 @@
 /**
  *  * main - Entry point
  *   *
- *    * Return: 0 to indicate successful program execution
+ *    * Return: Always 0 (Success)
  *     */
 int main(void)
 {
 	    return (0);
 }
-
