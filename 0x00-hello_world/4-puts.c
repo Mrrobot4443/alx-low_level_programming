2b27dc9dbacbea3300ed024e5d1403928ba0955c
YASSINE BESS <yazo-yazo@hotmail.com>
2023-06-16 08:29:16 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
-#!/bin/bash
 #include<stdio.h>
+/*
+ *print me this function 
+ *and return 0
+*/
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
