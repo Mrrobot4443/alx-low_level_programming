295ae87e40c5bfe79ed7ce38bc5d45f054ac21c1
YASSINE BESS <yazo-yazo@hotmail.com>
2023-06-16 17:18:02 -0700

    goo

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - alphabets lowercase
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char low[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(low[i]);
+	}
+	putchar('\n');
+	return (0);
