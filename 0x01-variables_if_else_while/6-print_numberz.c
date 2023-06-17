f39a2573c8c4c7354e104aedf14118cdac7d5e5d
YASSINE BESS <yazo-yazo@hotmail.com>
2023-06-16 18:34:51 -0700

    9

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - Prints numbers from 0 to 9.
+ * main - 0 to 9
  *
- * Return: Always 0 (Success)
+ * Return: 0
  */
 int main(void)
 {
--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - Prints numbers between 0 to 9 and letters between a to f.
+ * main - between 0-9.
  *
- * Return: Always 0 (Success)
+ * Return: 0
  */
 int main(void)
 {
