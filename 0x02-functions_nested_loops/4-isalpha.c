e5fbb29fcc5adba6fae9df4260eb29d76d2b59d7
YASSINE BESS <yazo-yazo@hotmail.com>
2023-06-20 16:16:45 -0700

    char loop

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isalpha - Start
+ * @c: check
+ * Return: 1 if c true, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+		return (1);
+	else
+		return (0);
+}
