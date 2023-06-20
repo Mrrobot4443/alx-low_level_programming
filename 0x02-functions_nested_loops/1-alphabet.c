081b02746d989f83e6bac87cebd472cacebf27e1
YASSINE BESS <yazo-yazo@hotmail.com>
2023-06-20 13:07:05 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - start
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	int Y;
+
+	for (Y = 97; Y <= 122 ; Y++)
+	{
+		_putchar(Y);
+	}
+	_putchar('\n');
+}
