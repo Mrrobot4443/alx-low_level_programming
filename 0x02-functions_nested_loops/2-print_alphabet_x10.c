cecfa8cb2ec38680b940a8ba37543f42c3009b33
YASSINE BESS <yazo-yazo@hotmail.com>
2023-06-20 14:17:50 -0700

    1

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,12 +8,12 @@ void print_alphabet_x10(void)
 	int Y;
 	int S;
 
-	for (S = 1; Y <= 10; Y++)
+	for (S = 1; S <= 10; S++)
 	{
 		for (Y = 97; Y <= 122; Y++)
 		{
-			_putchar(S);
+			_putchar(Y);
 		}
-	}
 		_putchar('\n');
+	}
 }
