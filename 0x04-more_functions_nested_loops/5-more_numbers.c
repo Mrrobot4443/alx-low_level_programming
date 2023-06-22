ad1f292691908f210bd40fb07e7bda51c03ed976
YASSINE BESS <yazo-yazo@hotmail.com>
2023-06-22 13:33:53 -0700

    5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * more_numbers- start
+ * Return: 0
+ */
+void more_numbers(void)
+{
+	int a, j;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+			_putchar(j % 10);
+		}
+	 _putchar('\n');
+	}
+}
