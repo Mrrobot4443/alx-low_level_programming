566223f7a00aaefd77c9b2117127d997c06228e2
YASSINE BESS <yazo-yazo@hotmail.com>
2023-06-22 12:04:29 -0700

    4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_most_numbers- start
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		if (a == 2 || a == 4)
+			continue;
+		_putchar(a);
+	}
+		_putchar('\n');
+}
