b67bbeae0fa684917cf5f7cf21b96f6ef4f1a624
YASSINE BESS <yazo-yazo@hotmail.com>
2023-06-27 15:26:45 -0700

    0-1

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts - function print string
+ * @str: string
+ * Return:lengh of str
+ */
+void _puts(char *str)
+{
+	int = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
