6453c37e6e3749e1b7e9af51282e7b814f3009d8
YASSINE BESS <yazo-yazo@hotmail.com>
2023-07-03 12:15:44 -0700

    0

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,13 +1,16 @@
 #include "main.h"
 /**
+  * main - start
   * _abs - Function blabla Start
   * @n: declaration
   * Return:0
   */
-
-int _abs(int n)
+int main(void)
 {
+int _abs(int n)
+	{
 	if (n <= 0)
 	n = (-1) * n;
 	return (n);
+	}
 }
